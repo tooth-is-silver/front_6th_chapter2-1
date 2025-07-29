c98279ff9acf324e3229d6ed861fcf8eec189acd
hmyo2853 <hmyo2853@gmail.com>
2025-07-29 23:37:32 +0900

    Feature : 할인율 계산 함수 분리

--- src/basic/main.basic.js
+++ src/basic/main.basic.js
@@ -11,6 +11,11 @@ import {
   getLowStockItems,
   generateStockMessage,
 } from "./data/products.js";
+import {
+  calculateProductDiscount,
+  calculateTuesdayDiscount,
+  calculateBulkDiscount,
+} from "./services/discount.js";
 
 let prodList;
 let bonusPts = 0;
@@ -24,39 +29,8 @@ let totalAmt = 0;
 let cartDisp;
 
 
-// 상품별 할인율 계산
-function calculateProductDiscount(product, quantity) {
-  if (quantity < BULK_ITEM_COUNT) return 0;
 
-  switch (product.id) {
-    case PRODUCT_IDS.KEYBOARD:
-      return DISCOUNT_RATES.KEYBOARD;
-    case PRODUCT_IDS.MOUSE:
-      return DISCOUNT_RATES.MOUSE;
-    case PRODUCT_IDS.MONITOR_ARM:
-      return DISCOUNT_RATES.MONITOR_ARM;
-    case PRODUCT_IDS.LAPTOP_POUCH:
-      return DISCOUNT_RATES.LAPTOP_POUCH;
-    case PRODUCT_IDS.SPEAKER:
-      return DISCOUNT_RATES.SPEAKER;
-    default:
-      return 0;
-  }
-}
 
-// 화요일 할인 계산
-function calculateTuesdayDiscount(totalAmount, isTuesday) {
-  if (!isTuesday || totalAmount <= 0) return 0;
-  return totalAmount * DISCOUNT_RATES.TUESDAY_DISCOUNT;
-}
-
-// 대량구매 할인 계산
-function calculateBulkDiscount(subtotal, totalQuantity) {
-  if (totalQuantity >= 30) {
-    return subtotal * DISCOUNT_RATES.BULK_DISCOUNT;
-  }
-  return 0;
-}
 
 // 포인트 계산
 function calculatePoints(totalAmount, cartItems, totalQuantity, isTuesday) {
