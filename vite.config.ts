060fcbd3df406b32f4b7b950a6cfb32f02a805b0
hmyo2853 <hmyo2853@gmail.com>
2025-08-01 05:50:38 +0900

    Feature : vite.config.ts 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  root: ".",
+  base: "/front_6th_chapter2-1/",
+  build: {
+    rollupOptions: {
+      input: "./index.html",
+    },
+    outDir: "dist",
+    copyPublicDir: true,
+  },
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: 'src/setupTests.js'
+    environment: "jsdom",
+    setupFiles: "src/setupTests.js",
   },
-})
+});
